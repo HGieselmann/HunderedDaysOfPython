56a85a98f815df05bc5bcabd5acb48f7144ffeb4
Henrik Gieselmann <henrik@henrikgieselmann.com>
2021-10-21 16:08:57 +0200

    Finished Day 6

---
+++ day06-functionsKarel/L61Challenge-RandomHurdles.py
@@ -0,0 +1,27 @@
+# Works at:
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def master_hurdle():
+#     turn_left()
+#     move()
+#     turn_right()
+#     if front_is_clear():
+#         move_down_hurdle()
+#
+# def move_down_hurdle():
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         master_hurdle()

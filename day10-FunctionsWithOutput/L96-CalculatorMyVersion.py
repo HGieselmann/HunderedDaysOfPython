5457ef4c48103ff67e425a5787eb00f8654be27a
Henrik Gieselmann <henrik@henrikgieselmann.com>
2021-10-25 21:09:16 +0200

    Added my version of the calculator app.

---
+++ day10-FunctionsWithOutput/L96-CalculatorMyVersion.py
@@ -0,0 +1,49 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return  a-b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+continue_calculating = True
+
+
+print("Welcome to the calculator program! ")
+use_intermediate_result = False
+while continue_calculating:
+    operation = input("Please enter a an operand to select the operation you want to do: (+,-,*,/)")
+    if use_intermediate_result:
+        input_a = intermediate_result
+    else:
+        input_a = float(input("Please enter the first value: "))
+    use_intermediate_result = False
+    input_b = float(input("Please enter the second value: "))
+
+    if operation == "+":
+        intermediate_result = add(input_a, input_b)
+    elif operation == "-":
+        intermediate_result = subtract(input_a, input_b)
+    elif operation == "*":
+        intermediate_result = multiply(input_a, input_b)
+    elif operation == "/":
+        intermediate_result = divide(input_a, input_b)
+    else:
+        print("Operation input invalid.")
+
+    print(f"Your result is {intermediate_result}.")
+    user_input = input("Enter 'c' to continue with this result, 'n' to begin a new calculation, 'exit' to end.")
+    if user_input == 'c':
+        use_intermediate_result = True
+    elif user_input == 'exit':
+        continue_calculating = False
+
+
+print("Thanks for using the simple python calculator.")
+
+
+

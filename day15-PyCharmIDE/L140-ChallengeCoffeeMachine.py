d394dfaf8c7c540e4a54cbc1bbd4d78959412bbd
Henrik Gieselmann <henrik@henrikgieselmann.com>
2021-10-30 22:56:44 +0200

    Initialized the day 15 Coffee making script

---
+++ day15-PyCharmIDE/L140-ChallengeCoffeeMachine.py
@@ -0,0 +1,8 @@
+# TODO Prompt User what he likes
+# TODO Turn off coffee machine
+# TODO Print report
+# TODO Check resources sufficient
+# TODO Process coins
+# TODO Check transactions successful
+# TODO Make Coffee
+

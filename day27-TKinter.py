6cbf691e840d639346b02b09fa97196730d26601
Henrik Gieselmann <henrik@henrikgieselmann.com>
2021-11-12 22:39:08 +0100

    Started TKinter

---
+++ day27-TKinter.py
@@ -0,0 +1,14 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Super GUI")
+window.minsize(width=640, height=480)
+
+title_label = tkinter.Label(text="Super GUI", font=("Courier", 16, "bold"))
+title_label.pack()
+
+
+
+
+
+window.mainloop()
